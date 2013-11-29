2478f5e7555828e41d2573ccb4a9c81d45e648a1
sambatyon <alexander.rojas@gmail.com>
2013-11-29 08:39:23 +0100

    A more C++ 11 version of problem 03

--- problem003.cc
+++ problem003.cc
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <cstdint>
 #include <cmath>
 #include <vector>
 
 std::vector<unsigned long long> primes(const unsigned long long &max);
 
 int main(int argc, char **argv) {
-  const unsigned long long number = 600851475143LLU;
-  unsigned long long sq = std::sqrt(number);
-  std::vector<unsigned long long> pr = primes(sq);
+  const auto number = 600851475143LLU;
+  auto sq = static_cast<std::uint64_t>(std::sqrt(number));
+  std::vector<std::uint64_t> pr = primes(sq);
   for (auto it = pr.rbegin(); it != pr.rend(); ++it) {
     if (number % *it == 0) {
       std::cout << *it << '\n';
@@ -20,17 +21,17 @@ int main(int argc, char **argv) {
 /**
  * Uses the primes generator algorithm from TAOCP vol. 1
  */
-std::vector<unsigned long long> primes(const unsigned long long &max) {
+std::vector<std::uint64_t> primes(const std::uint64_t &max) {
   unsigned n = 3;
-  std::vector<unsigned long long> primes;
+  std::vector<std::uint64_t> primes;
   primes.push_back(2);
   primes.push_back(n);
   while (n <= max) {
     n += 2;
-    for (unsigned k = 1; k < primes.size(); ++k) {
-      if (n % primes[k] == 0)
+    for (const auto &p : primes) {
+      if (n % p == 0)
         break;
-      if (n / primes[k] <= primes[k]) {
+      if (n / p <= p) {
         primes.push_back(n);
         break;
       }
